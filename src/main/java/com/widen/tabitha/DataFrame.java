13e8f1f3ece968ba63642a8860932e9584743543
Stephen M. Coakley <me@stephencoakley.com>
2017-04-13 16:32:28 -0500

    Add streaming data frames

--- src/main/java/com/widen/tabitha/DataFrame.java
+++ src/main/java/com/widen/tabitha/DataFrame.java
@@ -2,23 +2,76 @@ package com.widen.tabitha;
 
 import com.widen.tabitha.collections.RingBuffer;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Optional;
 
 /**
- * An in-memory ordered series of data rows.
+ * A mutable, ordered series of data rows.
+ * <p>
+ * Data frames can also be used as a lazy buffer for row readers. A data frame created from a row reader is called a
+ * <i>streaming</i> data frame. Rows are read from the reader lazily and only when needed to fulfil the result of a
+ * query.
+ * <p>
+ * This class has no methods for getting a list of the columns stored in the frame. This is because columns are stored
+ * per-cell instead of for a whole data set. While this offers greater flexibility, as a result, each row in a frame can
+ * contain different columns than each other and there is no way for a data frame to know all the possible columns it
+ * may contain.
  */
-public class DataFrame
+public class DataFrame implements Iterable<Row>, Closeable
 {
     private final RingBuffer<Row> rows;
+    private RowReader rowReader;
 
+    /**
+     * Create a new streaming data frame from a row reader.
+     *
+     * @param rowReader The row reader used to stream in data.
+     * @return A new data frame.
+     */
+    public static DataFrame streaming(RowReader rowReader)
+    {
+        DataFrame dataFrame = new DataFrame();
+        dataFrame.rowReader = rowReader;
+
+        return dataFrame;
+    }
+
+    /**
+     * Create a new in-memory data frame.
+     */
     public DataFrame()
     {
         rows = new RingBuffer<>();
+        rowReader = null;
     }
 
+    /**
+     * Create a new in-memory data frame pre-filled with the given rows.
+     *
+     * @param rows The rows to put in the data frame.
+     */
     public DataFrame(Row... rows)
     {
         this.rows = new RingBuffer<>(rows);
+        rowReader = null;
+    }
+
+    /**
+     * Checks if this data frame is a streaming data frame.
+     */
+    public boolean isStreaming()
+    {
+        return rowReader != null;
+    }
+
+    /**
+     * Checks if the data frame is currently empty.
+     */
+    public boolean isEmpty()
+    {
+        return rows.isEmpty();
     }
 
     /**
@@ -31,14 +84,43 @@ public class DataFrame
 
     /**
      * Get a row by index.
+     * <p>
+     * If this is a streaming data frame, rows will be read if possible until the given index is reachable.
+     *
+     * @param index The index of the row to get.
+     * @return The row, or none if the index does not exist.
      */
     public Optional<Row> get(int index)
     {
+        if (isStreaming())
+        {
+            while (index >= size())
+            {
+                try
+                {
+                    Row row = rowReader.read().orElse(null);
+
+                    if (row == null)
+                    {
+                        break;
+                    }
+
+                    pushBack(row);
+                }
+                catch (IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
         return rows.get(index);
     }
 
     /**
      * Add a row to the beginning of the frame.
+     *
+     * @param row The row to push
      */
     public void pushFront(Row row)
     {
@@ -47,6 +129,8 @@ public class DataFrame
 
     /**
      * Add a row to the end of the frame.
+     *
+     * @param row The row to push
      */
     public void pushBack(Row row)
     {
@@ -55,36 +139,94 @@ public class DataFrame
 
     /**
      * Remove a row from the beginning of the frame.
+     * <p>
+     * If this is a streaming data frame and the data frame is empty, a row will be read and returned if possible.
+     *
+     * @return The removed row, or none if the data frame is empty.
      */
     public Optional<Row> popFront()
     {
+        if (isEmpty() && isStreaming())
+        {
+            try
+            {
+                return rowReader.read();
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
         return rows.popFront();
     }
 
     /**
      * Remove a row from the end of the frame.
+     * <p>
+     * If this is a streaming data frame and the data frame is empty, a row will be read and returned if possible.
+     *
+     * @return The removed row, or none if the data frame is empty.
      */
     public Optional<Row> popBack()
     {
+        if (isEmpty() && isStreaming())
+        {
+            try
+            {
+                return rowReader.read();
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
         return rows.popBack();
     }
 
     /**
-     * Create a row reader for this data frame. Note that the new reader will drain this frame of values until it is
-     * empty while reading.
+     * Create a row reader for this data frame.
+     *
+     * @return The row reader.
      */
     public RowReader reader()
     {
-        // This creates a row reader using a method reference as the implementation for read().
-        return this::popFront;
+        return RowReader.from(iterator());
     }
 
     /**
      * Create a row writer for this data frame. Rows written will be appended to the end of the frame.
+     *
+     * @return The row writer.
      */
     public RowWriter writer()
     {
         // This creates a row writer using a method reference as the implementation for write().
         return this::pushBack;
+    }
+
+    @Override
+    public Iterator<Row> iterator()
+    {
+        return rows.iterator();
+    }
+
+    /**
+     * Close the data frame.
+     * <p>
+     * If this is a streaming data frame, the underlying reader will be closed and no more rows will be read. This
+     * method has no effect on non-streaming data frames.
+     *
+     * @throws IOException
+     */
+    @Override
+    public void close() throws IOException
+    {
+        if (rowReader != null)
+        {
+            rowReader.close();
+            rowReader = null;
+        }
     }
 }
--- src/test/groovy/com/widen/tabitha/DataFrameTest.groovy
+++ src/test/groovy/com/widen/tabitha/DataFrameTest.groovy
@@ -1,7 +1,99 @@
 package com.widen.tabitha
 
-/**
- * Created by scoakley on 4/11/17.
- */
-class DataFrameTest {
+import spock.lang.*
+
+class DataFrameTest extends Specification {
+    def "test is streaming"() {
+        setup:
+        def inMemory = new DataFrame()
+        def streaming = DataFrame.streaming(RowReader.EMPTY)
+
+        expect:
+        !inMemory.isStreaming()
+        streaming.isStreaming()
+    }
+
+    def "test push and pop"() {
+        setup:
+        def row = Mock(Row.class)
+        def dataFrame = new DataFrame()
+
+        expect:
+        !dataFrame.popFront().isPresent()
+        dataFrame.pushBack(row)
+        dataFrame.popFront().get() == row
+        !dataFrame.popFront().isPresent()
+    }
+
+    def "test reading from a frame"() {
+        setup:
+        def row1 = Mock(Row.class)
+        def row2 = Mock(Row.class)
+        def row3 = Mock(Row.class)
+        def dataFrame = new DataFrame(row1, row2, row3)
+        def reader = dataFrame.reader();
+
+        expect:
+        reader.read().get() == row1
+        reader.read().get() == row2
+        reader.read().get() == row3
+        !reader.read().isPresent()
+    }
+
+    def "test writing to a frame"() {
+        setup:
+        def row1 = Mock(Row.class)
+        def row2 = Mock(Row.class)
+        def row3 = Mock(Row.class)
+        def dataFrame = new DataFrame()
+        def writer = dataFrame.writer();
+
+        expect:
+        dataFrame.isEmpty()
+
+        writer.write(row1)
+        dataFrame.get(0).get() == row1
+
+        writer.write(row2)
+        dataFrame.get(1).get() == row2
+
+        writer.write(row3)
+        dataFrame.get(2).get() == row3
+    }
+
+    def "test streaming reads automatically"() {
+        setup:
+        def row1 = Mock(Row.class)
+        def row2 = Mock(Row.class)
+        def row3 = Mock(Row.class)
+        def row4 = Mock(Row.class)
+        def dataFrame = DataFrame.streaming(RowReader.from(row1, row2, row3, row4))
+
+        expect:
+        dataFrame.size() == 0
+        dataFrame.isEmpty()
+
+        dataFrame.popFront().get() == row1
+
+        dataFrame.size() == 0
+        dataFrame.isEmpty()
+
+        dataFrame.popBack().get() == row2
+
+        dataFrame.size() == 0
+        dataFrame.isEmpty()
+
+        dataFrame.get(0).get() == row3
+
+        dataFrame.size() == 1
+        !dataFrame.isEmpty()
+
+        dataFrame.popFront().get() == row3
+
+        dataFrame.size() == 0
+        dataFrame.isEmpty()
+
+        dataFrame.close()
+        !dataFrame.get(0).isPresent()
+    }
 }
