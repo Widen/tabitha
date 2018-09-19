0787de28d893cdf7af969dea96414001ac3487bb
Stephen M. Coakley <me@stephencoakley.com>
2018-09-19 13:55:10 -0500

    Fix tests

--- src/test/groovy/com/widen/tabitha/Helpers.groovy
+++ src/test/groovy/com/widen/tabitha/Helpers.groovy
@@ -1,11 +1,14 @@
 package com.widen.tabitha
 
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * Helper functions for tests.
  */
 class Helpers {
-    public static File getResourceFile(String name) {
-        return new File(getClass().getResource("/" + name).getFile())
+    public static Path getResourceFile(String name) {
+        return Paths.get(getClass().getResource("/" + name).getFile())
     }
 
     public static InputStream getResourceStream(String name) {
